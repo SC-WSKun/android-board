251bed80b9081b4b3236043eb5aa86c22218e360
kunnwei <540610423@qq.com>
2025-01-11 15:46:57 +0800

    feat: home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
@@ -17,11 +16,15 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  const [xiaxingLoaded] = useFonts({
+    XiaXing: require('../assets/fonts/Slidexiaxing-Regular.ttf')
+  })
+
   useEffect(() => {
-    if (loaded) {
+    if (xiaxingLoaded && loaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [xiaxingLoaded, loaded]);
 
   if (!loaded) {
     return null;
@@ -30,10 +33,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,68 @@
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("@/assets/images/home-bg.jpg")}
+        style={styles.backgroundImage}
+      >
+        <Text style={styles.title}>迎宾机器人</Text>
+
+        <View style={styles.buttonContainer}>
+          <Link style={styles.button} href="/communicate">
+            对话
+          </Link>
+          <Link style={styles.button} href="/navigation">
+            导航
+          </Link>
+          <Link style={styles.button} href="/buildmap">
+            建图
+          </Link>
+          <Link style={styles.button} href="/setting">
+            设置
+          </Link>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 50,
+  },
+  titleContainer: {
+    marginBottom: 50,
+  },
+  title: {
+    fontSize: 150,
+    color: 'white',
+    fontFamily: 'XiaXing',
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 50
+  },
+  button: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    marginVertical: 10,
+    borderRadius: 5,
+    color: "#171717",
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+});
