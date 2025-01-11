89f764576d97ed8e1c92057c33c845666190ce59
kunnwei <540610423@qq.com>
2025-01-11 17:29:16 +0800

    feat: imageContainer

---
+++ app/navigation.tsx
@@ -0,0 +1,12 @@
+import ImageContainer from "@/components/ui/ImageContainer";
+import { StyleSheet, ImageBackground, View, Text } from "react-native";
+
+export default function NavigationScreen() {
+  return (
+    <ImageContainer>
+      <Text>请选择您要前往的目的地：</Text>
+    </ImageContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/ui/ImageContainer.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren } from "react";
+import { ImageBackground, StyleSheet, View } from "react-native";
+
+type Props = PropsWithChildren<{}>;
+export default function ImageContainer({ children }: Props) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("@/assets/images/home-bg.jpg")}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.grayContainer}>{children}</View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 50,
+  },
+  grayContainer: {
+    width: "90%",
+    height: "90%",
+    backgroundColor: "rgba(255,255,255,0.85)",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
