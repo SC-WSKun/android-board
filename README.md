9bd5e8ec166beed3c9197e51097719c1b94236d3
kunnwei <540610423@qq.com>
2025-02-10 22:26:18 +0800

    feat: use react-native-skia instead react-native-canvas

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Welcome to your Expo app 👋
+# Android-Board For 351 Robot Control
 
-This is an [Expo](https://expo.dev) project created with
-[`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+用 [Expo](https://expo.dev) 搭的机器人安卓控制端
+
+## Todo
+- [x] 接入Foxglove Bridge与机器人进行通信
+- [ ] 完成地图绘制（用react-native-canvas写了一个，发现这玩意的imageData不少bug，现在换成react-native-skia进行尝试）
+- [ ] 完成地图交互（标点、导航）
+
 
 ## Get started
 
@@ -38,23 +43,3 @@ npm run reset-project
 
 This command will move the starter code to the **app-example** directory and
 create a blank **app** directory where you can start developing.
-
-## Learn more
-
-To learn more about developing your project with Expo, look at the following
-resources:
-
-- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into
-  advanced topics with our [guides](https://docs.expo.dev/guides).
-- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a
-  step-by-step tutorial where you'll create a project that runs on Android, iOS,
-  and the web.
-
-## Join the community
-
-Join our community of developers creating universal apps.
-
-- [Expo on GitHub](https://github.com/expo/expo): View our open source platform
-  and contribute.
-- [Discord community](https://chat.expo.dev): Chat with Expo users and ask
-  questions.
--- components/RobotMap.tsx
+++ components/RobotMap.tsx
@@ -1,92 +1,26 @@
 import { useDrawContext } from '@/store/drawContext'
 import { useGlobal } from '@/store/globalContext'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { View, Text } from 'react-native'
-import Canvas, {
-  Image as CanvasImage,
-  Path2D,
-  ImageData,
-} from 'react-native-canvas'
+import { useEffect, useState } from 'react'
+import { View } from 'react-native'
+import { Canvas, Circle, Group } from '@shopify/react-native-skia'
 
 interface IRobotMapProps {
   fn: any
 }
 
 export function RobotMap(props: IRobotMapProps) {
-  const canvas = useRef<Canvas | null>(null)
   const { callService } = useGlobal()
   const { drawingMap } = useDrawContext()
   const [mapInfo, setMapInfo] = useState<any>(undefined)
   const [mapData, setMapData] = useState<any>(undefined)
-  const [canvasSize, setCanvasSize] = useState({ width: 1000, height: 600 })
-  const [resizing, setResizing] = useState(true)
+  const width = 256
+  const height = 256
+  const r = width * 0.33
 
   // TODO: 重置地图监听
   const clearListener = () => {
     console.log('clearListener')
   }
-
-  // 渲染canvas
-  const renderCanvas = async () => {
-    if (!canvas.current) return
-    console.log('[RobotMap] render canvas')
-    const ctx = await canvas.current.getContext('2d')
-    ctx.clearRect(0, 0, canvasSize.width, canvasSize.height) // Clear the canvas before drawing
-    ctx
-      .getImageData(0, 0, canvasSize.width, canvasSize.height)
-      .then(imageData => {
-        if (!canvas.current) return
-        console.log('[RobotMap] render image data')
-        console.log('imageData:', imageData)
-        const data = Object.values(imageData.data)
-        const length = Object.keys(data).length
-        console.log(mapData)
-        for (let row = 0; row < canvasSize.height; row++) {
-          for (let col = 0; col < canvasSize.width; col++) {
-            const mapI = col + (canvasSize.height - 1 - row) * canvasSize.width
-            const val = mapData[mapI]
-            const i = (col + row * canvasSize.width) * 4
-
-            data[i] = val === 100 ? 0 : val === 0 ? 236 : 127
-            data[i + 1] = val === 100 ? 0 : val === 0 ? 236 : 127
-            data[i + 2] = val === 100 ? 0 : val === 0 ? 236 : 127
-            data[i + 3] = 236
-          }
-        }
-        console.log(data.length)
-        console.log(canvasSize.width)
-        console.log(canvasSize.height)
-        const imgData = new ImageData(
-          canvas.current,
-          data,
-          canvasSize.width,
-          canvasSize.height,
-        )
-        ctx.putImageData(imgData, 0, 0)
-      })
-      .catch(err => {
-        console.error('[RobotMap] render imageData error:', err)
-      })
-  }
-
-  // 这个canvas变换大小的时候会导致渲染丢失，也没找到对应的回调监听，先搞个500ms延迟用着
-  const waitingResizeCanvas = () => {
-    setResizing(true)
-    if (canvas.current) {
-      canvas.current.width = canvasSize.width
-      canvas.current.height = canvasSize.height
-    }
-    setTimeout(() => {
-      setResizing(false)
-    }, 500)
-  }
-
-  /**
-   * canvas大小变换时等待初始化
-   */
-  useEffect(() => {
-    waitingResizeCanvas()
-  }, [canvasSize])
 
   /**
    * 获取地图数据
@@ -115,29 +49,15 @@ export function RobotMap(props: IRobotMapProps) {
     fetchData()
   }, [drawingMap])
 
-  /**
-   * 地图数据处理，更新地图大小、渲染地图
-   */
-  useEffect(() => {
-    if (mapInfo) {
-      if (
-        mapInfo.width !== canvasSize.width ||
-        mapInfo.height !== canvasSize.height
-      ) {
-        console.log('[RobotMap] resize canvas')
-        setCanvasSize({ width: mapInfo.width, height: mapInfo.height })
-      }
-      renderCanvas()
-    }
-  }, [resizing, mapInfo])
-
   return (
     <View>
-      <Canvas
-        ref={(canvasRef: Canvas) => {
-          canvas.current = canvasRef
-        }}
-      />
+      <Canvas style={{ width, height }}>
+        <Group blendMode='multiply'>
+          <Circle cx={r} cy={r} r={r} color='cyan' />
+          <Circle cx={width - r} cy={r} r={r} color='magenta' />
+          <Circle cx={width / 2} cy={width - r} r={r} color='yellow' />
+        </Group>
+      </Canvas>
     </View>
   )
 }
