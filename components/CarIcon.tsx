6156207a7f9cb70b0b77502370a71a0cc390a26b
kunnwei <540610423@qq.com>
2025-03-13 15:27:10 +0800

    feat: support display car direction

--- components/CarIcon.tsx
+++ components/CarIcon.tsx
@@ -1,14 +1,35 @@
 import { CarPosition } from '@/hooks/useCar'
-import { Circle } from '@shopify/react-native-skia'
+import { Group, Oval, Path, Skia } from '@shopify/react-native-skia'
+import { useState } from 'react'
 
 interface ICarIcon {
   carPosition: CarPosition
 }
 
 export function CarIcon(props: ICarIcon) {
-  const { carPosition } = props
+  const {
+    carPosition: { yaw },
+  } = props
+  const cx = 500
+  const cy = 300
+  const carDirection = Skia.Path.Make()
+  carDirection.moveTo(cx, cy - 16)
+  carDirection.lineTo(cx - 6, cy - 6)
+  carDirection.lineTo(cx + 6, cy - 6)
+  carDirection.lineTo(cx, cy - 16)
+  carDirection.close()
 
   return (
-    <Circle cx={carPosition.x} cy={carPosition.y} r={4} color='lightblue' />
+    <Group>
+      <Path
+        path={carDirection}
+        color='blue'
+        origin={Skia.Point(cx, cy)}
+        transform={[
+          { rotateZ: (yaw * Math.PI) / 180 }, // 旋转（角度转弧度）a
+        ]}
+      />
+      <Oval x={496} y={296} width={8} height={8} color='lightblue' />
+    </Group>
   )
 }
