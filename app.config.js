e3e5944458a9e7fd94fae783a61a582dcadf97a7
kunnwei <540610423@qq.com>
2025-06-10 15:49:53 +0800

    chore: add apk config & remove expo plugin

--- app.config.js
+++ app.config.js
@@ -1,7 +1,6 @@
 require('ts-node/register')
-const withBaiduSpeech = require('./plugins/withBaiduSpeechSDK').default
 import appJson from './app.json'
 module.exports = {
   ...appJson.expo,
-  plugins: [withBaiduSpeech],
+  plugins: [],
 }
